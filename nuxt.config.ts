d162b78e0dab334222fdd89453ba25fff4090952
Boozy044 <boozy044@gmail.com>
2022-12-09 04:14:09 +0300

    feature: add tailwind config and add sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ["@nuxtjs/color-mode", "@nuxtjs/tailwindcss"],
 
-})
+  tailwindcss: {
+    cssPath: "~/assets/scss/tailwind.scss",
+    configPath: "~/tailwind.config.ts",
+    exposeConfig: false,
+    injectPosition: 0,
+    viewer: true,
+  },
+
+  colorMode: {
+    preference: "system", // default theme
+    dataValue: "theme", // activate data-theme in <html> tag
+    classSuffix: "",
+  },
+});
